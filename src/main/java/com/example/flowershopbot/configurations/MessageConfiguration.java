d7d235665744ef721c75a64c5c81203ba5008103
George <vrazog@gmail.com>
2023-08-13 17:55:32 +0000

    first java-bot commi2t

--- src/main/java/com/example/flowershopbot/configurations/MessageConfiguration.java
+++ src/main/java/com/example/flowershopbot/configurations/MessageConfiguration.java
@@ -19,19 +19,6 @@ public class MessageConfiguration {
 
     public void sendMessage() throws IOException {
 
-        System.out.println("");
-
-
-        OkHttpClient client = new OkHttpClient().newBuilder()
-                .build();
-        MediaType mediaType = MediaType.parse("application/json");
-        RequestBody body = RequestBody.create(mediaType, "{\n    \"recipient\": {\n        \"id\": 6567177600041771\n    },\n    \"message\": {\n        \"text\": \"Thanks!\"\n    }\n}");
-        Request request = new Request.Builder()
-                .url("https://graph.facebook.com/v2.6/me/messages?access_token=EAAC2kuYox3wBO9sdaE7gtJJu0MKKqKwW87uScCCJNLmXXoZAsqurLSMe7FLeAuJVrYKUrFJEMShxjKbiNWnf88wQNhBZBK6ASXOSQ7xp7FzmhRpgIgi9IctgAfYR2vmteCqCXQSYCjtNyUVNKIIZBn3tZASdEkkP29EnExbL8RQbmGbYmQsXmD4dMu8u9Gps")
-                .method("POST", body)
-                .addHeader("Content-Type", "application/json")
-                .build();
-        Response response = client.newCall(request).execute();
 
 
 
